04301b57ff2d99f4f7fd5d81991efcbc5e29f57f
Eugeneycchoy <eugeneycchoy@gmail.com>
2025-03-24 11:42:57 +0800

    Responsiveness, UI improvements

--- js/learning_res.js
+++ js/learning_res.js
@@ -2,19 +2,64 @@
 let learnMoreBtn = document.querySelector(".learn-more-btn");
 let headerNum = document.querySelector("#header-num-count");
 let featureStatNums = document.querySelectorAll(".feature-stat");
-
 let tiltedCard = document.querySelector(".tilted-card");
-
 // keep track of previous scroll position
 let prevScrollPos = window.pageYOffset;
+
+// Event Listeners
+window.addEventListener("scroll", function () {
+  // current scroll position
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    document.querySelector("nav").classList.remove("hide");
+  } else {
+    // user has scrolled down
+    document.querySelector("nav").classList.add("hide");
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
 
 learnMoreBtn.addEventListener("click", function () {
   smoothScroll(".course-overview-section");
 });
 
-countUp(headerNum, 20, 90);
-executeOnce();
+// Add the scroll event listener
+window.addEventListener("scroll", main);
 
+// Select all carousel items
+const carouselItems = document.querySelectorAll(".carousel-item");
+// Select all carousel overlays
+const carouselOverlays = document.querySelectorAll(".carousel-overlay");
+// Select all carousel-text
+const carouselTexts = document.querySelectorAll(
+  ".course-highlights-text-content"
+);
+// Add event listeners to each carousel item
+carouselItems.forEach((carouselItem) => {
+  carouselItem.addEventListener("mouseover", () => {
+    carouselOverlays.forEach((carouselOverlay) => {
+      carouselOverlay.classList.add("remove-opacity");
+    });
+    carouselTexts.forEach((carouselText) => {
+      carouselText.classList.add("remove-opacity");
+    });
+  });
+
+  carouselItem.addEventListener("mouseout", () => {
+    carouselOverlays.forEach((carouselOverlay) => {
+      carouselOverlay.classList.remove("remove-opacity");
+    });
+    carouselTexts.forEach((carouselText) => {
+      carouselText.classList.remove("remove-opacity");
+    });
+  });
+});
+
+// Functions
 function smoothScroll(sectionQuery) {
   // Find the section using the provided CSS selector and scroll to it smoothly
   document.querySelector(sectionQuery).scrollIntoView({
@@ -41,17 +86,21 @@ function countUp(target, targetNum, intervalinMs) {
   }, intervalinMs);
 }
 
-function executeOnce() {
+/**
+ * Checks if the current page is index.html and the user has scrolled to the desired position,
+ * and if so, calls the countAllNums function to animate the feature statistics, and removes
+ * the scroll event listener.
+ * @return {void}
+ */
+function main() {
   // Check if the current page is index.html
   const path = window.location.pathname;
   if (path.endsWith("/") || path.endsWith("/index.html")) {
     // Get the current scroll position
     let scrollPosition = window.scrollY || document.documentElement.scrollTop;
-
     // Define the position where you want the function to execute
-    let overviewSectionPos = 850;
-    let featureSectionPos = 1200; // Change this value to your desired position
-
+    const overviewSectionPos = 850;
+    const featureSectionPos = 1200; // Change this value to your desired position
     // Check if the current scroll position has reached the target position
     if (scrollPosition >= featureSectionPos) {
       // Begin the "numbers going up" animation
@@ -65,11 +114,15 @@ function executeOnce() {
   }
 }
 
-// Add the scroll event listener
-window.addEventListener("scroll", executeOnce);
-
+/**
+ * Iterate over the featureStatNums array or NodeList and call countUp
+ * for each element with the correct parameters.
+ * @param {Array<HTMLElement>|NodeList} featureStatNums - The elements
+ *   to animate by calling countUp.
+ * @return {void}
+ */
 function countAllNums() {
-  // Ensure featureStatNums is defined and is an iterable
+  // Ensure featureStatNums is an array or NodeList
   if (
     Array.isArray(featureStatNums) ||
     NodeList.prototype.isPrototypeOf(featureStatNums)
@@ -77,6 +130,7 @@ function countAllNums() {
     for (let featureStatNum of featureStatNums) {
       // Ensure featureStatNum has a textContent property
       if (featureStatNum && featureStatNum.textContent) {
+        // Call countUp for each element with the correct parameters
         countUp(featureStatNum, featureStatNum.textContent, 35);
       } else {
         console.error("Invalid featureStatNum or missing textContent");
@@ -87,21 +141,6 @@ function countAllNums() {
   }
 }
 
-window.addEventListener("scroll", executeOnce);
-
-// Show navbar when scrolling up, hide when scrolling down
-window.addEventListener("scroll", function () {
-  // current scroll position
-  const currentScrollPos = window.pageYOffset;
-
-  if (prevScrollPos > currentScrollPos) {
-    // user has scrolled up
-    document.querySelector("nav").classList.remove("hide");
-  } else {
-    // user has scrolled down
-    document.querySelector("nav").classList.add("hide");
-  }
-
-  // update previous scroll position
-  prevScrollPos = currentScrollPos;
-});
+// Function Calls
+countUp(headerNum, 20, 90);
+main();
